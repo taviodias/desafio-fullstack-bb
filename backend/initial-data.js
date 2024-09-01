fcb94c97a10ca0073a37341bc562a49b655e28df
Otavio Dias <otaviodias@gmail.com>
2024-09-01 15:11:42 -0400

    refactor: refatoração do tratamento dos dados da SWAPI

--- backend/initial-data.js
+++ backend/initial-data.js
@@ -1,19 +1,36 @@
 import axios from "axios";
 import { PATHS } from "./constants.js";
 
-async function fetchPeople(){
+async function fetchPeople() {
     const results = [];
     let res, next;
-    next = PATHS.swapi+'people';
-    do{
+    next = PATHS.swapi + 'people';
+    do {
         res = await axios.get(next);
         next = res.data.next;
         results.push(...res.data.results);
-    }while(next)
-    
-    return results.map((person) =>{
+    } while (next)
+
+    return results.map((person) => {
+        const genders = {
+            male: 'Masculino',
+            female: 'Feminino',
+            hermaphrodite: 'Hermafrodita',
+            'n/a': 'n/a'
+        };
+
         const id = Number(person.url.match(/\d+/)[0]);
-        
+        if (person.height === 'unknown') {
+            person.height = '??';
+        }
+        if (person.mass === 'unknown') {
+            person.mass = '??';
+        }
+        if (person["birth_year"] === 'unknown') {
+            person["birth_year"] = '??';
+        }
+        person.gender = genders[person.gender];
+
         return {
             id,
             name: person.name,
@@ -21,7 +38,7 @@ async function fetchPeople(){
             mass: person.mass,
             birthYear: person["birth_year"],
             gender: person.gender,
-            avatar: PATHS.avatar+`${id}.jpg`
+            avatar: PATHS.avatar + `${id}.jpg`
         }
     });
 }
